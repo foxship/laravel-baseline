e33ed56d8235327a1567033950d0471360eb303f
Leo Unglaub <leo@unglaub.at>
2022-02-28 22:42:51 +0100

    Fresh start

---
+++ src/LaravelBaselineServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Foxship\LaravelBaseline;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBaselineServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+
+	public function boot(): void
+	{
+	}
+}
---
+++ src/Middleware/HttpDisableCache.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Foxship\LaravelBaseline\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HttpDisableCache
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+	 *
+	 * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+	 */
+	public function handle(Request $request, Closure $next)
+	{
+		/*
+		 * @var \Illuminate\Http\Response
+		 */
+		$response = $next($request);
+		$response->headers->set('Pragma', 'no-cache');
+		$response->headers->set('Cache-Control', 'no-store, no-cache, max-age=-1');
+		$response->headers->set('Expires', '-1');
+
+		return $response;
+	}
+}
